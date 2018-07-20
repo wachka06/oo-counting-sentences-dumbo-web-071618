5969a35ab8a94c380150dbd20dba4ccee7f4b5c5
wachka06 <wachka06@gmail.com>
2018-07-20 12:19:09 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,49 @@ require 'pry'
 
 class String
 
+  # attr_accessor :string
+
   def sentence?
-    
+    if self.split("").pop == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self[self.length-1] == "?"
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
 
+    # self.split(" ").length
+
+    string_array = self.split(" ")
+    count = 0
+    string_array.each do |word|
+      if word.sentence? || word.question? || word.exclamation?
+        count += 1
+      end
+    end
+    count
   end
+
 end
+
+# This is how the instance of String looks like...
+
+# a = String.new("mango")
+# a => "mango"
